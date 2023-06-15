5d3985fa57bdf8a4bff720fad7c9c688a4a39dce
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-06-15 17:40:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2
+console.log(multiply)
 
-// Write your code here
+let random = math.floor(math.random()*2)
+let num3 = 28;
+let num4 = 6;
+
+let mod = (num3 % num4) + 4;
+
+
+
