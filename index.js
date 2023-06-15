77ab37b46bed080170bd4adc3a74ed718cca493c
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-06-15 19:31:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ let num2 = 2
 let multiply = num1*num2
 console.log(multiply)
 
-let random = math.floor(math.random()*2)
-let num3 = 28;
-let num4 = 6;
+const random = Math.floor(Math.random()*10)+1;
+ let num3 = 64;
+ let num4 = 10;
+ let mod = num3 % num4
 
-let mod = (num3 % num4) + 4;
+ const max = Math.max(20, 16, 14, 10, 7, 5, 1)
+
 
 
 
